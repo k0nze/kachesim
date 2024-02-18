b044b11563960858b7ccfd838b65ea7a860ed2b1
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2024-02-18 16:42:01 +0100

    replaced set and get method of Data class with templated method.

--- src/data.h
+++ src/data.h
@@ -1,61 +1,49 @@
 #ifndef DATA_H
 #define DATA_H
 
+#include <algorithm>
 #include <cstdint>
-#include <vector>
 
 class Data {
 public:
     Data(uint32_t size);
+    ~Data();
 
-    uint32_t size();
+    size_t size();
 
     uint8_t operator[](uint64_t index) const { return data_[index]; }
     uint8_t& operator[](uint64_t index) { return data_[index]; }
 
-    void set_byte(uint8_t byte);
-    void set_half_word(uint16_t half_word);
-    void set_word(uint32_t word);
-    void set_double_word(uint64_t double_word);
+    template <typename T>
+    void set(T bytes, size_t offset = 0) {
+        int max_byte = std::min({(size_t)sizeof(T), size_});
 
-    void set_int8_t(int8_t a);
-    void set_uint8_t(uint8_t a);
+        for (int i = offset; i < max_byte; i++) {
+            data_[i] = (bytes >> (8 * i)) & 0xff;
+        }
 
-    void set_int16_t(int16_t a);
-    void set_uint16_t(uint16_t a);
+        // erase remaining bytes
+        for (int i = max_byte; i < size_; i++) {
+            data_[i] = 0;
+        }
+    }
 
-    void set_int32_t(int32_t a);
-    void set_uint32_t(uint32_t a);
+    template <typename T>
+    T get(size_t offset = 0) {
+        int max_byte = std::min({(size_t)sizeof(T), size_});
 
-    void set_int64_t(int64_t a);
-    void set_uint64_t(uint64_t a);
+        T bytes = 0;
 
-    void set_float(float f);
-    void set_double(double d);
+        for (int i = offset; i < max_byte; i++) {
+            bytes |= ((T)data_[i]) << (8 * i);
+        }
 
-    uint8_t get_byte();
-    uint16_t get_half_word();
-    uint32_t get_word();
-    uint64_t get_double_word();
-
-    int8_t get_int8_t();
-    uint8_t get_uint8_t();
-
-    int16_t get_int16_t();
-    uint16_t get_uint16_t();
-
-    int32_t get_int32_t();
-    uint32_t get_uint32_t();
-
-    int64_t get_int64_t();
-    uint64_t get_uint64_t();
-
-    float get_float();
-    double get_double();
+        return bytes;
+    }
 
 private:
-    uint32_t size_;
-    std::vector<uint8_t> data_;
+    size_t size_;
+    uint8_t* data_;
 };
 
 #endif
--- tests/test_data.cc
+++ tests/test_data.cc
@@ -7,231 +7,231 @@
 int main() {
     auto d1 = std::make_unique<Data>(1);
 
-    d1->set_double_word(0xbeef1234abcdef42);
-    assert(d1->get_double_word() == 0x42);
+    d1->set<uint64_t>(0xbeef1234abcdef42);
+    assert(d1->get<uint64_t>() == (uint64_t)0x42);
 
-    d1->set_word(0xbeef1234);
-    assert(d1->get_word() == 0x34);
+    d1->set<uint32_t>(0xbeef1234);
+    assert(d1->get<uint32_t>() == (uint32_t)0x34);
 
-    d1->set_half_word(0xbeef);
-    assert(d1->get_half_word() == 0xef);
+    d1->set<uint16_t>(0xbeef);
+    assert(d1->get<uint16_t>() == (uint16_t)0xef);
 
-    d1->set_byte(0xab);
-    assert(d1->get_byte() == 0xab);
+    d1->set<uint8_t>(0xab);
+    assert(d1->get<uint8_t>() == (uint8_t)0xab);
 
-    d1->set_int8_t(-12);
-    assert(d1->get_int8_t() == -12);
+    d1->set<int8_t>(-12);
+    assert(d1->get<int8_t>() == (int8_t)-12);
 
-    d1->set_uint8_t(12);
-    assert(d1->get_uint8_t() == 12);
+    d1->set<uint8_t>(12);
+    assert(d1->get<uint8_t>() == (uint8_t)12);
 
-    d1->set_int16_t(-4796);
-    assert(d1->get_int16_t() == (-4796 & 0xff));
+    d1->set<int16_t>(-4796);
+    assert(d1->get<int16_t>() == (-4796 & 0xff));
 
-    d1->set_uint16_t(4796);
-    assert(d1->get_uint16_t() == (4796 & 0xff));
+    d1->set<uint16_t>(4796);
+    assert(d1->get<uint16_t>() == (4796 & 0xff));
 
-    d1->set_int32_t(-71914);
-    assert(d1->get_int32_t() == (-71914 & 0xff));
+    d1->set<int32_t>(-71914);
+    assert(d1->get<int32_t>() == (-71914 & 0xff));
 
-    d1->set_uint32_t(71914);
-    assert(d1->get_uint32_t() == (71914 & 0xff));
+    d1->set<uint32_t>(71914);
+    assert(d1->get<uint32_t>() == (71914 & 0xff));
 
-    d1->set_int64_t(-4723642012);
-    assert(d1->get_int32_t() == (-4723642012 & 0xff));
+    d1->set<int64_t>(-4723642012);
+    assert(d1->get<int32_t>() == (-4723642012 & 0xff));
 
-    d1->set_uint64_t(4723642012);
-    assert(d1->get_uint32_t() == (4723642012 & 0xff));
+    d1->set<uint64_t>(4723642012);
+    assert(d1->get<uint32_t>() == (4723642012 & 0xff));
 
     auto d2 = std::make_unique<Data>(2);
 
-    d2->set_double_word(0xbeef1234abcdef42);
-    assert(d2->get_double_word() == 0xef42);
+    d2->set<uint64_t>(0xbeef1234abcdef42);
+    assert(d2->get<uint64_t>() == (uint64_t)0xef42);
 
-    d2->set_word(0xbeef1234);
-    assert(d2->get_word() == 0x1234);
+    d2->set<uint32_t>(0xbeef1234);
+    assert(d2->get<uint32_t>() == (uint32_t)0x1234);
 
-    d2->set_half_word(0xbeef);
-    assert(d2->get_half_word() == 0xbeef);
+    d2->set<uint16_t>(0xbeef);
+    assert(d2->get<uint16_t>() == (uint16_t)0xbeef);
 
-    d2->set_byte(0xab);
-    assert(d2->get_byte() == 0x00ab);
+    d2->set<uint8_t>(0xab);
+    assert(d2->get<uint8_t>() == (uint8_t)0x00ab);
 
-    d2->set_int8_t(-12);
-    assert(d2->get_int8_t() == -12);
+    d2->set<int8_t>(-12);
+    assert(d2->get<int8_t>() == (int8_t)-12);
 
-    d2->set_uint8_t(12);
-    assert(d2->get_uint8_t() == 12);
+    d2->set<uint8_t>(12);
+    assert(d2->get<uint8_t>() == (uint8_t)12);
 
-    d2->set_int16_t(-4796);
-    assert(d2->get_int16_t() == (int16_t)(-4796 & 0xffff));
+    d2->set<int16_t>(-4796);
+    assert(d2->get<int16_t>() == (int16_t)(-4796 & 0xffff));
 
-    d2->set_uint16_t(4796);
-    assert(d2->get_uint16_t() == (4796 & 0xffff));
+    d2->set<uint16_t>(4796);
+    assert(d2->get<uint16_t>() == (uint16_t)(4796 & 0xffff));
 
-    d2->set_int32_t(-71914);
-    assert(d2->get_int32_t() == (int32_t)(-71914 & 0xffff));
+    d2->set<int32_t>(-71914);
+    assert(d2->get<int32_t>() == (int32_t)(-71914 & 0xffff));
 
-    d2->set_uint32_t(71914);
-    assert(d2->get_uint32_t() == (71914 & 0xffff));
+    d2->set<uint32_t>(71914);
+    assert(d2->get<uint32_t>() == (uint32_t)(71914 & 0xffff));
 
-    d2->set_int64_t(-4723642012);
-    assert(d2->get_int64_t() == (int64_t)(-4723642012 & 0xffff));
+    d2->set<int64_t>(-4723642012);
+    assert(d2->get<int64_t>() == (int64_t)(-4723642012 & 0xffff));
 
-    d2->set_uint64_t(4723642012);
-    assert(d2->get_uint64_t() == (4723642012 & 0xffff));
+    d2->set<uint64_t>(4723642012);
+    assert(d2->get<uint64_t>() == (uint64_t)(4723642012 & 0xffff));
 
     auto d3 = std::make_unique<Data>(3);
 
-    d3->set_double_word(0xbeef1234abcdef42);
-    assert(d3->get_double_word() == 0xcdef42);
+    d3->set<uint64_t>(0xbeef1234abcdef42);
+    assert(d3->get<uint64_t>() == 0xcdef42);
 
-    d3->set_word(0xbeef1234);
-    assert(d3->get_word() == 0x00ef1234);
+    d3->set<uint32_t>(0xbeef1234);
+    assert(d3->get<uint32_t>() == (uint32_t)0x00ef1234);
 
-    d3->set_half_word(0xbeef);
-    assert(d3->get_half_word() == 0x00beef);
+    d3->set<uint16_t>(0xbeef);
+    assert(d3->get<uint16_t>() == (uint16_t)0x00beef);
 
-    d3->set_byte(0xab);
-    assert(d3->get_byte() == 0x0000ab);
+    d3->set<uint8_t>(0xab);
+    assert(d3->get<uint8_t>() == (uint8_t)0x0000ab);
 
-    d2->set_int8_t(-12);
-    assert(d2->get_int8_t() == -12);
+    d2->set<int8_t>(-12);
+    assert(d2->get<int8_t>() == -12);
 
-    d3->set_uint8_t(12);
-    assert(d3->get_uint8_t() == 12);
+    d3->set<uint8_t>(12);
+    assert(d3->get<uint8_t>() == 12);
 
-    d3->set_int16_t(-4796);
-    assert(d3->get_int16_t() == (int16_t)(-4796 & 0xffffff));
+    d3->set<int16_t>(-4796);
+    assert(d3->get<int16_t>() == (int16_t)(-4796 & 0xffffff));
 
-    d3->set_uint16_t(4796);
-    assert(d3->get_uint16_t() == (4796 & 0xffffff));
+    d3->set<uint16_t>(4796);
+    assert(d3->get<uint16_t>() == (4796 & 0xffffff));
 
-    d3->set_int32_t(-71914);
-    assert(d3->get_int32_t() == (int32_t)(-71914 & 0xffffff));
+    d3->set<int32_t>(-71914);
+    assert(d3->get<int32_t>() == (int32_t)(-71914 & 0xffffff));
 
-    d3->set_uint32_t(71914);
-    assert(d3->get_uint32_t() == (71914 & 0xffffff));
+    d3->set<uint32_t>(71914);
+    assert(d3->get<uint32_t>() == (71914 & 0xffffff));
 
-    d3->set_int64_t(-4723642012);
-    assert(d3->get_int64_t() == (int64_t)(-4723642012 & 0xffffff));
+    d3->set<int64_t>(-4723642012);
+    assert(d3->get<int64_t>() == (int64_t)(-4723642012 & 0xffffff));
 
-    d3->set_uint64_t(4723642012);
-    assert(d3->get_uint64_t() == (4723642012 & 0xffffff));
+    d3->set<uint64_t>(4723642012);
+    assert(d3->get<uint64_t>() == (4723642012 & 0xffffff));
 
     auto d4 = std::make_unique<Data>(4);
 
-    d4->set_double_word(0xbeef1234abcdef42);
-    assert(d4->get_double_word() == 0xabcdef42);
+    d4->set<uint64_t>(0xbeef1234abcdef42);
+    assert(d4->get<uint64_t>() == 0xabcdef42);
 
-    d4->set_word(0xbeef1234);
-    assert(d4->get_word() == 0xbeef1234);
+    d4->set<uint32_t>(0xbeef1234);
+    assert(d4->get<uint32_t>() == 0xbeef1234);
 
-    d4->set_half_word(0xbeef);
-    assert(d4->get_half_word() == 0x0000beef);
+    d4->set<uint16_t>(0xbeef);
+    assert(d4->get<uint16_t>() == 0x0000beef);
 
-    d4->set_byte(0xab);
-    assert(d4->get_byte() == 0x000000ab);
+    d4->set<uint8_t>(0xab);
+    assert(d4->get<uint8_t>() == 0x000000ab);
 
-    d4->set_int8_t(-12);
-    assert(d4->get_int8_t() == -12);
+    d4->set<int8_t>(-12);
+    assert(d4->get<int8_t>() == -12);
 
-    d4->set_uint8_t(12);
-    assert(d4->get_uint8_t() == 12);
+    d4->set<uint8_t>(12);
+    assert(d4->get<uint8_t>() == 12);
 
-    d4->set_int16_t(-4796);
-    assert(d4->get_int16_t() == -4796);
+    d4->set<int16_t>(-4796);
+    assert(d4->get<int16_t>() == -4796);
 
-    d4->set_uint16_t(4796);
-    assert(d4->get_uint16_t() == 4796);
+    d4->set<uint16_t>(4796);
+    assert(d4->get<uint16_t>() == 4796);
 
-    d4->set_int32_t(-71914);
-    assert(d4->get_int32_t() == -71914);
+    d4->set<int32_t>(-71914);
+    assert(d4->get<int32_t>() == -71914);
 
-    d4->set_uint32_t(71914);
-    assert(d4->get_uint32_t() == 71914);
+    d4->set<uint32_t>(71914);
+    assert(d4->get<uint32_t>() == 71914);
 
-    d4->set_int64_t(-4723642012);
-    assert(d4->get_int64_t() == (int64_t)(-4723642012 & 0xffffffff));
+    d4->set<int64_t>(-4723642012);
+    assert(d4->get<int64_t>() == (int64_t)(-4723642012 & 0xffffffff));
 
-    d4->set_uint64_t(4723642012);
-    assert(d4->get_uint64_t() == (4723642012 & 0xffffffff));
+    d4->set<uint64_t>(4723642012);
+    assert(d4->get<uint64_t>() == (4723642012 & 0xffffffff));
 
     auto d6 = std::make_unique<Data>(6);
 
-    d6->set_double_word(0xbeef1234abcdef42);
-    assert(d6->get_double_word() == 0x1234abcdef42);
+    d6->set<uint64_t>(0xbeef1234abcdef42);
+    assert(d6->get<uint64_t>() == 0x1234abcdef42);
 
-    d6->set_word(0xbeef1234);
-    assert(d6->get_word() == 0x0000beef1234);
+    d6->set<uint32_t>(0xbeef1234);
+    assert(d6->get<uint32_t>() == 0x0000beef1234);
 
-    d6->set_half_word(0xbeef);
-    assert(d6->get_half_word() == 0x00000000beef);
+    d6->set<uint16_t>(0xbeef);
+    assert(d6->get<uint16_t>() == 0x00000000beef);
 
-    d6->set_byte(0xab);
-    assert(d6->get_byte() == 0x0000000000ab);
+    d6->set<uint8_t>(0xab);
+    assert(d6->get<uint8_t>() == 0x0000000000ab);
 
-    d6->set_int8_t(-12);
-    assert(d6->get_int8_t() == -12);
+    d6->set<int8_t>(-12);
+    assert(d6->get<int8_t>() == -12);
 
-    d6->set_uint8_t(12);
-    assert(d6->get_uint8_t() == 12);
+    d6->set<uint8_t>(12);
+    assert(d6->get<uint8_t>() == 12);
 
-    d6->set_int16_t(-4796);
-    assert(d6->get_int16_t() == -4796);
+    d6->set<int16_t>(-4796);
+    assert(d6->get<int16_t>() == -4796);
 
-    d6->set_uint16_t(4796);
-    assert(d6->get_uint16_t() == 4796);
+    d6->set<uint16_t>(4796);
+    assert(d6->get<uint16_t>() == 4796);
 
-    d6->set_int32_t(-71914);
-    assert(d6->get_int32_t() == -71914);
+    d6->set<int32_t>(-71914);
+    assert(d6->get<int32_t>() == -71914);
 
-    d6->set_uint32_t(71914);
-    assert(d6->get_uint32_t() == 71914);
+    d6->set<uint32_t>(71914);
+    assert(d6->get<uint32_t>() == 71914);
 
-    d6->set_int64_t(-4723642012);
-    assert(d6->get_int64_t() == (int64_t)(-4723642012 & 0xffffffffffff));
+    d6->set<int64_t>(-4723642012);
+    assert(d6->get<int64_t>() == (int64_t)(-4723642012 & 0xffffffffffff));
 
-    d6->set_uint64_t(4723642012);
-    assert(d6->get_uint64_t() == (4723642012 & 0xffffffffffff));
+    d6->set<uint64_t>(4723642012);
+    assert(d6->get<uint64_t>() == (4723642012 & 0xffffffffffff));
 
     auto d8 = std::make_unique<Data>(8);
 
-    d8->set_double_word(0xbeef1234abcdef42);
-    assert(d8->get_double_word() == 0xbeef1234abcdef42);
+    d8->set<uint64_t>(0xbeef1234abcdef42);
+    assert(d8->get<uint64_t>() == (uint64_t)0xbeef1234abcdef42);
 
-    d8->set_word(0xbeef1234);
-    assert(d8->get_word() == 0x00000000beef1234);
+    d8->set<uint32_t>(0xbeef1234);
+    assert(d8->get<uint32_t>() == 0x00000000beef1234);
 
-    d8->set_half_word(0xbeef);
-    assert(d8->get_half_word() == 0x000000000000beef);
+    d8->set<uint16_t>(0xbeef);
+    assert(d8->get<uint16_t>() == 0x000000000000beef);
 
-    d8->set_byte(0xab);
-    assert(d8->get_byte() == 0x00000000000000ab);
+    d8->set<uint8_t>(0xab);
+    assert(d8->get<uint8_t>() == 0x00000000000000ab);
 
-    d8->set_int8_t(-12);
-    assert(d8->get_int8_t() == -12);
+    d8->set<int8_t>(-12);
+    assert(d8->get<int8_t>() == -12);
 
-    d8->set_uint8_t(12);
-    assert(d8->get_uint8_t() == 12);
+    d8->set<uint8_t>(12);
+    assert(d8->get<uint8_t>() == 12);
 
-    d8->set_int16_t(-4796);
-    assert(d8->get_int16_t() == -4796);
+    d8->set<int16_t>(-4796);
+    assert(d8->get<int16_t>() == -4796);
 
-    d8->set_uint16_t(4796);
-    assert(d8->get_uint16_t() == 4796);
+    d8->set<uint16_t>(4796);
+    assert(d8->get<uint16_t>() == 4796);
 
-    d8->set_int32_t(-71914);
-    assert(d8->get_int32_t() == -71914);
+    d8->set<int32_t>(-71914);
+    assert(d8->get<int32_t>() == -71914);
 
-    d8->set_uint32_t(71914);
-    assert(d8->get_uint32_t() == 71914);
+    d8->set<uint32_t>(71914);
+    assert(d8->get<uint32_t>() == 71914);
 
-    d8->set_int64_t(-4723642012);
-    assert(d8->get_int64_t() == -4723642012);
+    d8->set<int64_t>(-4723642012);
+    assert(d8->get<int64_t>() == -4723642012);
 
-    d8->set_uint64_t(4723642012);
-    assert(d8->get_uint64_t() == 4723642012);
+    d8->set<uint64_t>(4723642012);
+    assert(d8->get<uint64_t>() == 4723642012);
 
     return 0;
 }

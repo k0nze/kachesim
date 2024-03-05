82e636491c6ed5d08957b10551bf8465e699dd36
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2024-03-05 07:59:10 +0100

    Added test_data_storage_transaction to weed out bug.

---
+++ tests/test_data_storage_transaction.cc
@@ -0,0 +1,30 @@
+#include <cassert>
+#include <iostream>
+
+#include "data_storage_transaction.h"
+
+DataStorageTransaction test_function() {
+    Data data = Data(9);
+
+    for (int i = 0; i < 9; i++) {
+        data[i] = i + 1;
+    }
+
+    DataStorageTransaction dst = {READ, 0x0042, 23, 1, data};
+    return dst;
+}
+
+int main() {
+    DataStorageTransaction dst = test_function();
+
+    assert(dst.type == READ);
+    assert(dst.address == 0x0042);
+    assert(dst.latency == 23);
+    assert(dst.hit_level == 1);
+
+    for (int i = 0; i < 9; i++) {
+        assert(dst.data[i] == i + 1);
+    }
+
+    return 0;
+}

7ef7fefe98c0dc63c34ffc5fbd43a3d2c7edd67a
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2024-05-04 10:23:16 +0200

    Added check for MemoryHierarchy::data_storage_dependency_map_.

--- src/memory_hierarchy.cc
+++ src/memory_hierarchy.cc
@@ -2,6 +2,7 @@
 
 #include <yaml-cpp/yaml.h>
 
+#include <algorithm>
 #include <iostream>
 
 namespace kachesim {
@@ -11,57 +12,71 @@ MemoryHierarchy::MemoryHierarchy() = default;
 MemoryHierarchy::MemoryHierarchy(const std::string& yaml_config_string) {
     YAML::Node config = YAML::Load(yaml_config_string);
 
-    // access nodes
-    if (config["nodes"]) {
-        auto nodes = config["nodes"];
+    // access data_storages
+    if (config["data_storages"]) {
+        auto data_storages = config["data_storages"];
 
         // check if nodes is a sequence
-        if (!nodes.IsSequence()) {
-            throw std::runtime_error("'nodes' in yaml config is not a sequence");
+        if (!data_storages.IsSequence()) {
+            throw std::runtime_error(
+                "'data_storages' in yaml config is not a sequence");
         }
 
-        // iterate over nodes in nodes
-        for (const auto& node : nodes) {
-            std::cout << "-" << std::endl;
+        // iterate over data_storages to collect names, types and dependencies
+        for (const auto& data_storage : data_storages) {
+            std::string data_storage_name_str;
+            std::string data_storage_type_str;
+
             // access node name
-            if (node["name"]) {
-                auto node_name = node["name"];
-                auto node_name_str = node_name.as<std::string>();
-                std::cout << node_name_str << std::endl;
+            if (data_storage["name"]) {
+                auto data_storage_name = data_storage["name"];
+                data_storage_name_str = data_storage_name.as<std::string>();
+                data_storage_names_.push_back(data_storage_name_str);
             } else {
                 throw std::runtime_error(
-                    "No node does not contain 'name' in yaml config");
+                    "No data_storage does not contain 'name' in yaml config");
             }
 
-            // access node type
-            if (node["type"]) {
-                auto node_type = node["type"];
-                auto node_type_str = node_type.as<std::string>();
-                std::cout << node_type_str << std::endl;
+            // access data storage type
+            if (data_storage["type"]) {
+                auto data_storage_type = data_storage["type"];
+                data_storage_type_str = data_storage_type.as<std::string>();
+                data_storage_type_map_.insert(
+                    {data_storage_name_str, data_storage_type_str});
             } else {
                 throw std::runtime_error(
-                    "No node does not contain 'type' in yaml config");
+                    "No data_storage does not contain 'type' in yaml config");
+            }
+
+            if (data_storage_type_str.compare("SetAssociativeCache") == 0) {
+                if (data_storage["next_level_data_storage"]) {
+                    data_storage_dependency_map_.insert(
+                        {data_storage_name_str,
+                         data_storage["next_level_data_storage"].as<std::string>()});
+
+                } else {
+                    throw std::runtime_error(
+                        "No data_storage of type SetAssociativeCache does not contain "
+                        "'next_level_data_storage' in yaml config");
+                }
+            }
+        }
+
+        // check data_storage_dependency_map_
+        for (const auto& [data_storages_name, next_level_data_storage_name] :
+             data_storage_dependency_map_) {
+            auto it = std::find(data_storage_names_.begin(), data_storage_names_.end(),
+                                next_level_data_storage_name);
+            if (it == data_storage_names_.end()) {
+                throw std::runtime_error("No data_storage with name " +
+                                         next_level_data_storage_name +
+                                         " in yaml config");
             }
         }
 
     } else {
         throw std::runtime_error("No 'nodes' in yaml config");
     }
-    /*
-    for (const auto& pair : config) {
-        std::string key = pair.first.as<std::string>();
-        std::cout << key << std::endl;
-        YAML::Node value = pair.second;
-
-        if(value.IsMap()) {
-            std::cout << "Map" << std::endl;
-        } else if(value.IsSequence()) {
-            std::cout << "Sequence" << std::endl;
-        } else {
-            std::cout << "Scalar" << std::endl;
-        }
-    }
-    */
 }
 
 DataStorageTransaction MemoryHierarchy::write(address_t address, Data& data) {
--- src/memory_hierarchy.h
+++ src/memory_hierarchy.h
@@ -1,6 +1,7 @@
 #ifndef MEMORY_HIERARCHY_H
 #define MEMORY_HIERARCHY_H
 
+#include <map>
 #include <string>
 
 #include "data_storage_transaction.h"
@@ -12,6 +13,11 @@ public:
     MemoryHierarchy(const std::string& yaml_config_string);
     DataStorageTransaction write(address_t address, Data& data);
     DataStorageTransaction read(address_t address, size_t num_bytes);
+
+private:
+    std::vector<std::string> data_storage_names_;
+    std::map<std::string, std::string> data_storage_type_map_;
+    std::map<std::string, std::string> data_storage_dependency_map_;
 };
 }  // namespace kachesim
 
